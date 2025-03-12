2dfe4f4c6fc38b493fee9cbb2a586d34a418a99e
Nath <nathlej41@gmail.com>
2025-03-12 11:44:00 +0100

    i

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,9 +34,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="task"
         options={{
-          title: 'Explore',
+          title: 'task',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
---
+++ app/(tabs)/task.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import tw from 'twrnc';
+
+
+export default function Task() {
+    return (
+        <Text style={tw`text-white`}>hello world</Text>
+    );
+}
+
+
